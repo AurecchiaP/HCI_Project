7adc82dff3aa6e5a88eeaafe5a2ac66bc75829dc
Lucas Pennati <pennal@usi.ch>
2015-05-28 12:11:16 +0200

    Image Fetching is working

--- HistoryOfInformatics/main.py
+++ HistoryOfInformatics/main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import request,make_response,abort
+from flask import request,make_response,abort,send_file
 from flask import Flask #Flask is the base server. use 'sudo pip3 install Flask' to install
 import os
 import common
@@ -13,6 +13,11 @@ app = Flask(__name__,static_url_path='')
 @app.route("/<section>/<article>")
 def mainPage(section,article):
     return article_generator.create_article(section,article)
+    
+@app.route("/<section>/images/<filename>")
+def returnImage(section,filename):
+    pathToImage=str("static/images/" + filename)
+    return send_file(pathToImage)
 
 # Small demo for the current time in unix timestamp
 @app.route("/curepochdate")
